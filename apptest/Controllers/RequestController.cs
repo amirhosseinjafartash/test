f1b07206e4615806a8a28937f417cbf046befee2
amirhosseinjafartash <www.amirhossein.jafartash@gmail.com>
2021-07-25 17:31:10 +0430

    first commit

---
+++ apptest/Controllers/RequestController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using apptest.Models;
+
+namespace apptest.Controllers
+{
+    public class RequestController : ApiController
+    {
+        #region post:kala,user,sabad
+        [HttpPost]
+        public IHttpActionResult GetRequest_Addkala1(string name,int price)
+        {
+            KalaController kala = new KalaController();
+            kala.insertkal(name, price);
+            return Ok(kala);
+        }
+        [HttpPost]
+        public IHttpActionResult GetRequest_AddUser1(string name,int shomare)
+        {
+            UserController user = new UserController();
+            user.insertuser(name,shomare);
+            return Ok(user);
+        }
+        [HttpPost]
+        public IHttpActionResult addsabad(string name, string kala)
+        {
+            SabadKalaController sbd = new SabadKalaController();
+            sbd.insertsabad(name, kala);
+            return Ok(sbd);
+        }
+
+        #endregion
+
+        #region get: user,sabad,kala
+        [HttpGet]
+        public IHttpActionResult GetRequest_Showuser()
+        {
+            List<User> user = new List<User>();
+            user = UserController.show();
+            return Ok(user);
+        }
+        [HttpGet]
+        public IHttpActionResult GetRequest_sabad(string name)
+        {
+            List<Sabad> sabad = new List<Sabad>();
+            sabad = SabadKalaController.show1(name);
+            return Ok(sabad);
+        }
+        [HttpGet]
+        public IHttpActionResult GetRequest_sabaduser(string name)
+        {
+            List<Sabad> sabad = new List<Sabad>();
+            sabad = SabadKalaController.show2(name);
+            return Ok(sabad);
+        }
+        #endregion
+    }
+}
---
+++ apptest/Controllers/SabadKalaController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using apptest.Models;
+using apptest.Controllers;
+
+namespace apptest.Controllers
+{
+    public class SabadKalaController : ApiController
+    {
+        public void insertsabad(string name,string kala)
+        {
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.connection);
+            SqlCommand cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "insertsabad";
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@kala", kala);
+            cmd.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// نمایش افرادی که از فروشگاه خریداری کردند
+        /// </summary>
+        /// <param username="name"></param>
+        /// <returns></returns>
+        public static List<Sabad> show1(string name)
+        {
+            List<Sabad> sabadshow = new List<Sabad>();
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.connection);
+            SqlCommand cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "show_sabad";
+            cmd.Parameters.AddWithValue("@name", name);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Sabad sabad = new Sabad();
+                sabad.nameuser = dt.Rows[i]["U_Name"].ToString();
+                sabadshow.Add(sabad);
+            }
+            return sabadshow;
+        }
+        /// <summary>
+        /// اسم کالاهایی که از فروشگاه خریداری شد
+        /// </summary>
+        /// <param kala_name="name"></param>
+        /// <returns></returns>
+        public static List<Sabad> show2(string name)
+        {
+            List<Sabad> sabadshow = new List<Sabad>();
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.connection);
+            SqlCommand cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "showuser_sabad";
+            cmd.Parameters.AddWithValue("@namekala", name);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Sabad sabad = new Sabad();
+                sabad.nameuser = dt.Rows[i]["N_Kala"].ToString();
+                sabadshow.Add(sabad);
+            }
+            return sabadshow;
+        }
+    }
+    
+}
